40ce4db2e745cb9ed58560af4c135b8da97ac17d
Magino <magino.cochet@gmail.com>
2021-01-14 16:58:49 +0100

    fix module export and remove es6

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
  * Throw a Set to it and a function which can generate promises
  * and it will call them sequentially, one after another
  */
-export default (items, consumer) => {
+module.exports = function (items, consumer) {
     const results = [];
     const iterator = items.values();
-    const runner = () => {
+    const runner = function() {
         const item = iterator.next().value;
         if (item) {
         return consumer(item)
-            .then((result) => {
+            .then(function(result) {
             results.push(result);
             })
             .then(runner);
